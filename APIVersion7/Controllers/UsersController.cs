96feeaed43d86d2b4fb3d04bf2a85d3e34cd8d10
Vlad Probst <vladlen.probst@gmail.com>
2024-06-02 14:20:15 +0300

    End of section 2

---
+++ APIVersion7/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using APIVersion7.Data;
+using APIVersion7.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIVersion7.Controllers;
+
+[ApiController]
+[Route("[controller]")] // /users
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        List<AppUser> users = await _context.Users.ToListAsync();
+
+        return users;
+    }
+
+    [HttpGet("{id}")] // /api/users/2
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+         return await _context.Users.FindAsync(id);
+    }
+}
---
+++ APIVersion7/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using APIVersion7.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIVersion7.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+    
+    public DbSet<AppUser> Users { get; set; }
+}
