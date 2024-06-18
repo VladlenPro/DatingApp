251519f317261d8ec4579dbe542cd6f1255ef505
Vlad Probst <vladlen.probst@gmail.com>
2024-06-18 17:42:13 +0300

    end of section 7 error handling done

---
+++ APIVersion7/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Text.Json;
+using APIVersion7.Errors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIVersion7.Middleware;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionMiddleware> _logger;
+    private readonly IHostEnvironment _env;
+
+    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+    {
+        _next = next;
+        _logger = logger;
+        _env = env;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            string serverError = "Internal ServerError";   
+            _logger.LogError(ex, ex.Message);
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            ApiException response = _env.IsDevelopment() 
+            ? new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString()) 
+            : new ApiException(context.Response.StatusCode, ex.Message, serverError);
+
+            JsonSerializerOptions options = new JsonSerializerOptions{PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
+
+            string json = JsonSerializer.Serialize(response, options);
+
+            await context.Response.WriteAsync(json);
+
+        }
+    }
+}
--- APIVersion7/Program.cs
+++ APIVersion7/Program.cs
@@ -1,4 +1,5 @@
 using APIVersion7.Extensions;
+using APIVersion7.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,8 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseCors(builder => builder
 .AllowAnyHeader()
